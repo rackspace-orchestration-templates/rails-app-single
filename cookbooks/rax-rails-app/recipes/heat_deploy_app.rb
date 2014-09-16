48f9e948054ca514ab3c7bb1e0a6221053e831ba
Jason Boyles <jason@duncancreek.net>
2014-09-16 14:52:40 -0500

    update cookbooks

--- cookbooks/rax-rails-app/recipes/heat_deploy_app.rb
+++ cookbooks/rax-rails-app/recipes/heat_deploy_app.rb
@@ -4,6 +4,7 @@
 # When passed as a parameter from Heat, node['railsstack']['rails']['rake_tasks']
 # comes in as a string of space-separated words, but needs to be a list.
 node.set['railsstack']['rails']['rake_tasks'] = node['railsstack']['rails']['rake_tasks'].split(' ')
+node.set['railsstack']['additional_packages'] = node['railsstack']['additional_packages'].split(' ')
 
 # Heat parameters can't be nil, only an empty string, so convert an
 # empty string to nil so logic works as expected.
@@ -24,13 +25,13 @@ if platform_family?('debian')
   end
 end
 
-include_recipe "rax-rails-app::app_host_user"
+include_recipe 'rax-rails-app::app_host_user'
 include_recipe "rax-rails-app::ruby_#{node['railsstack']['ruby_manager']}"
 
 if node['railsstack']['git_url'].empty?
   include_recipe 'rax-rails-app::rails4_base_app'
 else
-  include_recipe "rax-rails-app::deploy_app"
+  include_recipe 'rax-rails-app::deploy_app'
 end
 
 include_recipe "rax-rails-app::app_server_#{node['railsstack']['app_server']}"
